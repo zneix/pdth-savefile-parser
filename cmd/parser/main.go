664e5428ef186ffc774326b934b1c69e4a63c46e
zneix <zneix@zneix.eu>
2023-07-01 03:56:09 +0200

    Add switch for printing json / cash+rep

--- cmd/parser/main.go
+++ cmd/parser/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"math"
 	"os"
 
 	"github.com/json-iterator/go"
@@ -46,10 +48,46 @@ func (sf *SaveFile) Contents() any {
 	return sf.gamedata.Data()
 }
 
+func reputation(skillProgress map[any]any, cash float32) float64 {
+	var rep float32
+
+	// proceed with regular levels
+	for _, v := range skillProgress {
+		rep += v.(float32)
+	}
+
+	// calculate virtual reputation
+	// vrep formula: https://github.com/steam-test1/pdth-source/blob/428335cb05cc26a697ac05fac9456742bf49e769/lib/tweak_data/tweakdata.lua#L1661
+	// floor((306750 + sqrt((306750^2) - 517000 * (1333214 - 3088000 - $cash + 101904000/47))) / 5500)
+	if (rep == 145 && cash > 700000) || (rep >= 193 && cash > 1333214) {
+		return math.Max(float64(rep), math.Floor((306750+math.Sqrt(math.Pow(306750, 2)-517000*(1333214-3088000-float64(cash)+101904000/47)))/5500))
+	}
+
+	return float64(rep)
+}
+
+var (
+	printJSON    *bool
+	savefileName *string
+)
+
+func init() {
+	printJSON = flag.Bool("json", false, "controls whether or not print JSON of all available game data")
+	savefileName = flag.String("file", "save099.sav", "path to the PAYDAY's savefile")
+}
+
 func main() {
-	savefile := NewSaveFile(os.Args[1])
+	flag.Parse()
+
+	savefile := NewSaveFile(*savefileName)
 	data := savefile.Contents().(map[any]any)
-	//fmt.Printf("%.0f\n", data["ExperienceManager"].(map[any]any)["total"])
+
+	if !*printJSON {
+		cash := data["ExperienceManager"].(map[any]any)["total"]
+		fmt.Printf("total cash: %.0f\n", cash)
+		fmt.Println("reputation:", reputation(data["UpgradesManager"].(map[any]any)["progress"].(map[any]any), cash.(float32)))
+		return
+	}
 
 	dataJSON, err := jsoniter.Config{
 		EscapeHTML:                    false,
